a59b898f78992186931e665b0eff627c3c54f31f
Davide64-dev <davidpcu@hotmail.com>
2024-05-14 15:37:32 +0100

    added menu

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Davide Teixeira on 14/05/2024.
+//
+
+#include "Menu.h"
---
+++ Menu.h
@@ -0,0 +1,14 @@
+//
+// Created by Davide Teixeira on 14/05/2024.
+//
+
+#ifndef DA_PROJ2_MENU_H
+#define DA_PROJ2_MENU_H
+
+
+class Menu {
+
+};
+
+
+#endif //DA_PROJ2_MENU_H
